0e2cf789c6ecac5f53be85106c3be0ff99fba39a
yassir <yassir.manaf@gmail.com>
2017-03-13 23:39:24 -0400

    salut c mon premier commit

---
+++ db.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+class Database:
+
+
+    def __init__(self):
+        self.connection = None
+
+    def get_connection(self):
+        if self.connection is None:
+            self.connection= sqlite3.connect('cms.bd')
+        return self.connection
+
+    def get_titres(self):
+        cursor= self.get_connection().cursor()
+        titres= cursor.execute("select titre from article").fetchall()
+        identifiant = cursor.execute("select identifiant from article").fetchall()
+        auteur = cursor.execute("select auteur from article").fetchall()
+        date=cursor.execute("select date_publication from article").fetchall()
+        parag=cursor.execute("select paragraphe from article").fetchall()
+        return [titre[0] for titre in titres]
+
+
+
+    def disconnect(self):
+        if self.connection is not None:
+            self.connection.close()
+
+    def insert_article(self,title, identifiant, author, publish_date, parag ):
+
+        connection = self.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(("insert into article(titre, identifiant, auteur, date_publication, paragraphe)"
+                        "values(?,?,?,?,?)"),(title, identifiant, author, publish_date, parag))
+        connection.commit()
+
+
+    
---
+++ index.py
@@ -0,0 +1,66 @@
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import request
+from db import Database
+from flask import g
+
+
+app = Flask (__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        g._database = Database()
+    return g._database
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.disconnect()
+
+@app.route('/')
+def index():
+    titres = get_db().get_titres()
+
+    return render_template('index.html', titres=titres )
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/admin-nouveau')
+def newPost():
+    return render_template('nouveau-article.html')
+
+@app.route('/error')
+def erreur():
+    return render_template('erreur.html')
+
+@app.route('/envoyer' , methods=['POST'])
+def formulaire():
+    titre= request.form['titre']
+    identifiant= request.form['idArticle']
+    auteur = request.form['auteur']
+    date = request.form['date']
+    paragraphe = request.form['paragraphe']
+
+    if '' in (titre , identifiant, auteur , date , paragraphe):
+        return render_template('nouveau-article.html',erreur='Tout les champs sont obligatoires', value=titre)
+
+
+
+    else:
+        get_db().insert_article(titre, identifiant, auteur, date, paragraphe)
+        return redirect('/thanks')
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
